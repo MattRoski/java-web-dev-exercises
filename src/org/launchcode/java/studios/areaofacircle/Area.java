7da08cbedb19b30e794e9cf90698027e4478f7de
MattRoski <41523468+MattRoski@users.noreply.github.com>
2022-04-11 20:32:39 -0500

    Studio Area Of Circle complete

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -3,16 +3,30 @@ package org.launchcode.java.studios.areaofacircle;
 import java.util.Scanner;
 
 public class Area {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean loop = true;
         System.out.println("Enter the radius of the circle");
-        double rad = input.nextDouble();
-        while(rad < 1 ){
-            System.out.println("Enter a valid input");
-            rad = input.nextDouble();
-        }
-        System.out.println(Circle.getArea(rad));
+
+
+            try {
+                while (loop) {
+                    double rad = input.nextDouble();
+                    if (rad <= 0) {
+                        System.out.println("Enter a non-negative number");
+                    } else {
+                        System.out.println(Circle.getArea(rad));
+                        loop = false;
+                    }
+                }
+
+
+            } catch (Exception error) {
+                System.out.println("Input mismatch. Must enter a number.");
+            }
+
     }
+
 
 
 }
